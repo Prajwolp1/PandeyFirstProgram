19b0673578b4c796f297831b7e275e3bdc9110a5
BT_1N3_12 <sample@bths.edu>
2023-09-21 08:36:19 -0400

    Asks how many people and what the tip percentage is.

---
+++ TipCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the Tip Calculator! ");
+
+        System.out.print("How may people are in your group: ");
+        int people = scan.nextInt();
+
+        System.out.print("What is your tip percentage? (0-100) ");
+
+
+}
