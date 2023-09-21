0106659020f5ba1458e7e156a872c98e82d2acd3
BT_1N3_12 <sample@bths.edu>
2023-09-21 08:43:46 -0400

    Creates the base while loop

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -8,6 +8,15 @@ public class TipCalculator {
         int people = scan.nextInt();
 
         System.out.print("What is your tip percentage? (0-100) ");
+        int tip = scan.nextInt();
+
+        int value = 0;
+        while (value != -1) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            double
+
+        }
 
 
+    }
 }
